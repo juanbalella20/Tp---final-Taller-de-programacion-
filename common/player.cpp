ccbd41a959da66984172b168ffbe2fd3ebe1460c
Mercedes González Lago <mmgonzalez@fi.uba.ar>
2026-05-13 19:46:43 -0300

    inicializaciones, mejora en nombre de funciones y nuevas funciones

--- common/player.cpp
+++ common/player.cpp
@@ -2,19 +2,50 @@
 
 
 Player::Player(PlayerRace player_race, PlayerClass player_class): 
+    status(PlayerStatus::ALIVE),
     player_race(player_race), 
     player_class(player_class), 
-    player_inventory() {}
+    player_inventory() {
 
-
-void Player::take_object(Item item) {
-    player_inventory.equip(item);
+    lives = MAX_LIVES;
+    gold = 0;
+    experience = 0;
+    mana = MAX_MANA;
 }
 
-void Player::drop_object(Item item) {
-    player_inventory.unequip(item);
+
+void Player::add_item(Item item) {
+    player_inventory.add_item(item);
+}
+
+void Player::drop_item(Item item) {
+    player_inventory.drop_item(item);
+}
+
+void Player::equip_item(Item item) {
+    player_inventory.equip_item(item);
+}
+
+void Player::unequip_item(Item item) {
+    player_inventory.unequip_item(item);
 }
 
 void Player::use_object(Item item) {
     item.use_item();
 }
+
+void Player::revive() {
+    lives = MAX_LIVES;
+    status = PlayerStatus::ALIVE;
+    experience = 0;
+}
+
+void Player::heal() {
+    lives = MAX_LIVES;
+    mana = MAX_MANA;
+}
+
+int Player::get_gold() {
+    return gold;
+}
+
