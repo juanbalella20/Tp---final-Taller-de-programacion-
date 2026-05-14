9fb5412d0466e37d0226304babfa4990193d5641
agus778 <agustintrombetta8@gmail.com>
2026-05-13 21:41:45 -0300

    add: implementacion npc banker

--- server/npcBanker.h
+++ server/npcBanker.h
@@ -7,30 +7,25 @@
 #include <vector>
 class NPCbanker: public NPCfriendly {
     private:
-        //<player_tag, items_vector>
-        std::map<int, std::vector<Item>> item_bank;
-        //<player_tag, gold>
-        std::map<int, int> gold_bank;
+        //<player_name, items_vector>
+        std::map<std::string, std::vector<Item>> item_bank;
+        //<player_name, gold>
+        std::map<std::string, int> gold_bank;
 
         // gets item bank
         std::map<int, std::vector<Item>> get_item_bank();
         // gets gold bank
         std::map<int, int> get_gold_bank();
-        // modifies item bank
-        void update_item_bank();
-        // modifies gold bank
-        void update_gold_bank();
-        
-        // gives item from item bank to player
-        Item withdraw_item();
+        // adds item bank
+        void receive_item(Player player, Item item);
+        // adds gold from player
+        void receive_gold(Player player, int ammount);
+        // transfers item to player and deletes it from bank
+        void transfer_item(Player player, Item item);
+        // transfers gold to player and subtracts it from bank
+        void transfer_gold(Player player, int ammount);
+    
 
-        // gives gold from gold bank to player
-        int withdraw_gold();
-
-        // saves item from player
-        void save_item();
-        // saves gold from player
-        void save_gold();
 
 
     public:
