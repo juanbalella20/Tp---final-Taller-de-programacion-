b19cb50c2b741dc838b948cf4ada621a0a31a563
agus778 <agustintrombetta8@gmail.com>
2026-05-15 13:46:25 -0300

    add: hostile npc

--- server/npcHostile.h
+++ server/npcHostile.h
@@ -2,30 +2,55 @@
 #define NPC_HOSTILE_H_
 
 #include "npc.h"
-#include <vector>
+#include "itemDataBase.h"
+
+enum class State { DEAD, ALIVE };
+
+
 
 class NPChostile : public NPC {
     private:
-        // (x,y)
-        std::vector<int> min_attack_distance;
         int type;
         int hp;
         int attack;
+        int max_hp;
+        State state;
+        ItemDataBase itemdatabase;
 
         NPChostile();
+        void set_hp(int hp);
+        int get_hp();
+        void set_attack(int attack);
+        int get_attack();
+        void set_type(int type);
+        void attack(Player player);
+        void receive_dmg(int dmg);
+        void death();
+        void set_state(State state);
+        // drops a potion
+        Item drop_potion();
+        // drops an item 
+        Item drop_item();
+        // drops gold
+        int drop_gold();
+        // evaluates what to drop: nothing, gold or item
+        void drop();
+        // todo
+        void respawn();
+        void set_max_hp(int hp);
+        int get_max_hp();
+
     public:
         // stats of each NPC depedns on creature and type
-        NPChostile set_goblin(int type);
-        NPChostile set_skeleton(int type);
-        NPChostile set_zombie();
-        NPChostile set_spider(int type);
-        NPChostile set_orc();
-        NPChostile set_golem(int type);
+        void set_goblin(int type);
+        void set_skeleton(int type);
+        void set_spider(int type);
+        void set_golem(int type);
+        void set_zombie();
+        void set_orc();
         
         // attacks player
-        void interact(Player player_tag) override;
-        // drops (or not) an item when slained 
-        Item drop_item();
+        void interact(Player player, Command cmd) override;
 };
 
 #endif 
