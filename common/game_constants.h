9a09545086ad350aa2d9b1ad6ab11edf0d05b8e8
agus778 <agustintrombetta8@gmail.com>
2026-05-17 18:01:05 -0300

    add: game constants

---
+++ common/game_constants.h
@@ -0,0 +1,24 @@
+#ifndef GAME_CONSTANTS_H
+#define GAME_CONSTANTS_H
+
+struct position_coord {
+    int x;
+    int y;
+};
+
+struct sectorPerimiter {
+    position_coord top_left;
+    position_coord top_right;
+    position_coord bottom_left;
+    position_coord bottom_right;
+};
+
+enum class elements {
+    players,
+    npcs,
+    objects,
+    buildings,
+    empty
+};
+
+#endif
