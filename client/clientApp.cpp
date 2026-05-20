4267f06a6f825385e65bc228c7974093c011c284
Mercedes González Lago <mmgonzalez@fi.uba.ar>
2026-05-20 09:22:04 -0300

    revertido

--- client/clientApp.cpp
+++ client/clientApp.cpp
@@ -18,7 +18,6 @@ void ClientApp::initialize_connection(ClientProtocol& protocol) {
     protocol.send_command(cmd);
 }
 
-
 void ClientApp::run() {
     Socket skt(host_.c_str(), port_.c_str());
     ClientProtocol protocol(std::move(skt));
