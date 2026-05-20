e05d4977bbe0f52d86d1f68a9dabe6811810b0e7
Mercedes González Lago <mmgonzalez@fi.uba.ar>
2026-05-20 17:50:11 -0300

    textura y update

--- client/minichat.cpp
+++ client/minichat.cpp
@@ -1,8 +1,11 @@
 #include "minichat.h"
 
 #include <SDL3/SDL.h>
+#include <SDL3_ttf/SDL_ttf.h>
 
-MiniChat::MiniChat(SDL_Renderer* renderer) : renderer(renderer) {}
+MiniChat::MiniChat(SDL_Renderer* renderer, TTF_Font* font) : 
+    renderer(renderer),
+    font(font) {}
 
 bool MiniChat::handle_event(const SDL_Event& event) {
     if (!is_active) {
@@ -47,4 +50,73 @@ void MiniChat::toggle_active() {
     } else {
         SDL_StopTextInput(window);
     }
+}
+
+SDL_Texture* MiniChat::create_texture_from_msg(const std::string& msg, float& width, float& height) {
+    if (msg.empty()) {
+        width = 0.0f;
+        height = 0.0f;
+        return nullptr;
+    }
+
+    SDL_Color msg_color = {255, 255, 255, 255};
+
+    SDL_Surface* surface = TTF_RenderText_Blended(font, msg.c_str(), 0, msg_color);
+
+    if (!surface) {
+        return nullptr;
+    }
+
+    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+
+    if (texture) {
+        SDL_GetTextureSize(texture, &width, &height);
+    }
+
+    SDL_DestroySurface(surface);
+
+    return texture;
+}
+
+void MiniChat::update(Queue<std::string>& inbox) {
+    std::string msg;
+
+    while (inbox.try_pop(msg)) {
+        float width = 0.0f;
+        float height = 0.0f;
+
+        SDL_Texture* texture = create_texture_from_msg(msg, width, height);
+
+        if (texture) {
+            chatMessage new_msg;
+            new_msg.text = msg;
+            new_msg.texture = texture;
+            new_msg.width = width;
+            new_msg.height = height;
+
+            msg_history.push_back(new_msg);
+
+            if (msg_history.size() > MAX_LINES) {
+                pop_oldest_message();
+            }
+        }
+    }
+}
+
+void MiniChat::pop_oldest_message() {
+    if (!msg_history.empty()) {
+        if (msg_history.front().texture) {
+            SDL_DestroyTexture(msg_history.front().texture);
+        }
+
+        msg_history.pop_front();
+    }
+}
+
+MiniChat::~MiniChat() {
+    while (!msg_history.empty()) {
+        pop_oldest_message();
+    }
+
+    TTF_CloseFont(font);
 }
--- client/minichat.h
+++ client/minichat.h
@@ -3,8 +3,16 @@
 
 #include <string>
 #include <SDL3/SDL.h>
+#include <SDL3_ttf/SDL_ttf.h>
 #include <deque>
 #include "../common/queue.h"
+
+struct chatMessage {
+    std::string text;
+    SDL_Texture* texture;
+    float width;
+    float height;
+}
 
 class MiniChat {
 
@@ -12,22 +20,29 @@ private:
     bool is_active;
     std::string player_input;
     std::string outbound_msg;
-    std::deque<std::string> msg_history;
+    std::deque<chatMessage> msg_history;
 
     SDL_Renderer* renderer;
+    TTF_Font* font;
+
+    void pop_oldest_message();
 
 public:
-    MiniChat(SDL_Renderer* renderer);
+    MiniChat(SDL_Renderer* renderer, TTF_Font* font);
 
     bool handle_event(const SDL_Event& event);
 
     void toggle_active();
+
+    SDL_Texture* create_texture_from_msg(const std::string& msg, float& width, float& height);
 
     bool has_pending_outbound_message() const;
 
     std::string pop_outbound_message();
 
     void update(Queue<std::string>& inbox);
+
+    ~MiniChat();
 };
 
 #endif
