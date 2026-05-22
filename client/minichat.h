8ed6d946664a788176061b17d8b3dc84b425cc34
Mercedes González Lago <mmgonzalez@fi.uba.ar>
2026-05-22 09:51:27 -0300

    comentarios para entender mejor qué hace cada función

--- client/minichat.h
+++ client/minichat.h
@@ -27,23 +27,56 @@ private:
     SDL_Renderer* renderer;
     TTF_Font* font;
 
+    /*
+    Si el chat supera MAX_LINES, elimina el mensaje más viejo
+    y destruye su textura de la memoria
+    */
     void pop_oldest_message();
 
 public:
     MiniChat(SDL_Renderer* renderer, TTF_Font* font);
 
+    /*
+    Si el chat está inactivo, no hace nada (false)
+    Si está activo:
+    Si es una letra, la suma a lo que se está escribiendo (player_input)
+    Si es Backspace, borra la última letra
+    Si es Enter, guarda el mensaje terminado en una variable temporal de salida (outbound_msg)
+    */
     bool handle_event(const SDL_Event& event);
 
+    /*
+    Si está activo, lo desactiva; y viceversa
+    Si está activo, llama a SDL_StartTextInput(window)
+    Si está inactivo, llama a SDL_StopTextInput(window)
+    */
     void toggle_active();
 
+    /*
+    Toma el string msg y lo dibuja en una imagen estática que la
+    placa de video pueda entender (SDL_Texture)
+    */
     SDL_Texture* create_texture_from_msg(const std::string& msg, float& width, float& height);
 
+    /*
+    Devuelve true si el jugador escribió algo nuevo, false en caso contrario
+    */
     bool has_pending_outbound_message() const;
 
+    /*
+    Devuelve el mensaje del jugador
+    */
     std::string pop_outbound_message();
 
+    /*
+    Saca uno por uno los mensajes que llegaron (inbox)
+    Los convierte en imágenes para guardarlos en el historial visual (msg_history)
+    */
     void update(Queue<std::string>& inbox);
 
+    /*
+    Dibuja y pega las texturas del historial en la pantalla
+    */
     void render();
 
     ~MiniChat();
