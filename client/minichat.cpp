acfdc3c640264d746e702531072770e79b70fe7e
Mercedes González Lago <mmgonzalez@fi.uba.ar>
2026-05-22 10:22:46 -0300

    evento usuario cierra la ventana y el chat está activo

--- client/minichat.cpp
+++ client/minichat.cpp
@@ -12,6 +12,10 @@ bool MiniChat::handle_event(const SDL_Event& event) {
         return false;
     }
 
+    if (event.type == SDL_EVENT_QUIT) {
+        return false;
+    }
+
     if (event.type == SDL_EVENT_KEY_DOWN) {
         switch (event.key.scancode) {
             case SDL_SCANCODE_ESCAPE:
