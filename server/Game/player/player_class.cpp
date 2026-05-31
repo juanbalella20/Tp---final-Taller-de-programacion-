5c39beb1cbdf24e4673b637060de21e7e73ef30d
Mercedes <mmgonzalez@fi.uba.ar>
2026-05-31 19:38:27 -0300

    agrego implementacion de setters

--- server/Game/player/player_class.cpp
+++ server/Game/player/player_class.cpp
@@ -1,7 +1,7 @@
 #include "player_class.h"
 
 int PlayerClass::class_constitution() {
-    return strength + endurance + agility;
+    return endurance;
 }
 
 int PlayerClass::class_inteligence() {
@@ -22,4 +22,40 @@ int PlayerClass::class_life_factor() {
 
 int PlayerClass::class_mana_factor() {
     return mana_factor;
+}
+
+void PlayerClass::set_wizard() {
+    inteligence = 18;
+    strength = 3;
+    agility = 8;
+    endurance = 4;
+    life_factor = 2;
+    mana_factor = 18;
+}
+
+void PlayerClass::set_cleric() {
+    inteligence = 14;
+    strength = 10;
+    agility = 10;
+    endurance = 12;
+    life_factor = 8;
+    mana_factor = 12;
+}
+
+void PlayerClass::set_paladin() {
+    inteligence = 10;
+    strength = 15;
+    agility = 10;
+    endurance = 15;
+    life_factor = 14;
+    mana_factor = 8;
+}
+
+void PlayerClass::set_warrior() {
+    inteligence = 3;
+    strength = 18;
+    agility = 12;
+    endurance = 18;
+    life_factor = 18;
+    mana_factor = 1;
 }
--- server/Game/player/player_race.cpp
+++ server/Game/player/player_race.cpp
@@ -1,7 +1,7 @@
 #include "player_race.h"
 
 int PlayerRace::race_constitution() {
-    return strength + endurance + agility;
+    return endurance;
 }
 
 int PlayerRace::race_inteligence() {
@@ -22,4 +22,40 @@ int PlayerRace::race_life_factor() {
 
 int PlayerRace::race_mana_factor() {
     return mana_factor;
+}
+
+void PlayerRace::set_human() {
+    inteligence = 10;
+    strength = 10;
+    agility = 10;
+    endurance = 10;
+    life_factor = 10;
+    mana_factor = 10;
+}
+
+void PlayerRace::set_elf() {
+    inteligence = 17;
+    strength = 5;
+    agility = 17;
+    endurance = 4;
+    life_factor = 4;
+    mana_factor = 16;
+}
+
+void PlayerRace::set_dwarf() {
+    inteligence = 6;
+    strength = 17;
+    agility = 4;
+    endurance = 17;
+    life_factor = 16;
+    mana_factor = 4;
+}
+
+void PlayerRace::set_gnome() {
+    inteligence = 14;
+    strength = 8;
+    agility = 8;
+    endurance = 14;
+    life_factor = 12;
+    mana_factor = 14;
 }
