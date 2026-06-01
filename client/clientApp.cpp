ea7473350d7cd7fedc9208a55192dfe1f8f1c852
Mercedes <mmgonzalez@fi.uba.ar>
2026-06-01 14:31:03 -0300

    la interfaz recibe la raza del jugador por constructor

--- client/clientApp.cpp
+++ client/clientApp.cpp
@@ -25,7 +25,7 @@ void ClientApp::run() {
 
     NetworkSenderThread sender(protocol, sendingQueue);
     NetworkReceiverThread receiver(protocol, receivingQueue);
-    ClientGUI gui(sendingQueue, receivingQueue, player_name_);
+    ClientGUI gui(sendingQueue, receivingQueue, player_name_, race_);
 
     sender.start();
     receiver.start();
