70948831c779290f8aa49fcd08cb549c15c24b8a
Ignacio <nachomahbalos@gmail.com>
2026-06-01 23:41:18 -0300

    Integro npc banker para poder depositar items

--- server/Game/npc/npcBanker.cpp
+++ server/Game/npc/npcBanker.cpp
@@ -1,65 +1,37 @@
 #include "npcBanker.h"
 #include <map>
 #include <vector>
+#include <iostream>
+#include "inventory.h"
+#include "../player/player.h"
 
-/*
-NPCbanker::NPCbanker() {
-    set_npc_name("Banker");
+NPCbanker::NPCbanker(int x, int y) : pos_x(x), pos_y(y) {}
+
+int NPCbanker::get_coord_x() const { return pos_x; }
+
+int NPCbanker::get_coord_y() const { return pos_y; }
+
+void NPCbanker::deposit_item(Player& player, const std::string& item_id) {
+    std::unique_ptr<Item> item = player.take_item_from_inventory(item_id);
+    item_bank[player.get_name()].push_back(std::move(item));
 }
 
-void NPCbanker::receive_item(Player player, Item item) {
-    item_bank.insert(player.get_name(), item);
+void NPCbanker::deposit_gold(Player& player, int amount) {
+    if (player.get_gold() < (uint32_t)amount)
+        throw std::runtime_error("No tenes suficiente oro.");
+    player.give_gold(-amount);
+    gold_bank[player.get_name()] += amount;
 }
 
-void NPCbanker::receive_gold(Player player, int ammount) {
-    if (!player.give_gold(ammount)) {
-        break;
-    }
-    auto it = gold_bank.find(player.get_name());
-    int actual_gold = it->first;
-    gold_bank.insert(player.get_name+ammount);
-}
-
-void NPCbanker::transfer_item(Player player, Item item) {
-    auto it = item_bank.find(player.get_name());
-    std::vector<Item> pl_items =  it->first;
-    for (auto it = pl_items.begin(); it != pl_items.end(); ++it) {
-        if (it = item) {
-            player.add_item(item);
-            pl_items.erase(current_position);
+void NPCbanker::interact(Player& player, Command cmd) {
+    switch (cmd.action) {
+        case ACTION_DEPOSIT:
+            deposit_item(player, cmd.item_id);
             break;
-        }
-    }
-}
-
-void NPCbanker::transfer_gold(Player player, int ammount) {
-    auto it = gold_bank.find(player.get_name());
-    int actual_gold = it->first;
-    if (ammount > actual_gold) {
-        // throw message to player = not enough money
-        break;
-    }
-    player.add_gold(ammount);
-    gold_bank.insert(player.get_name(), actual_gold-ammount);
-}
-
-
-void NPCbanker::interact(Player player, Command cmd) {
-    switch (cmd.type) {
-        case RECV_ITEM : 
-            receive_item(player,cmd.item);
-            break;
-        case RECV_GOLD : 
-            receive_gold(player,cmd.cantidad);
-            break;
-        case TR_ITEM :
-            transfer_item(player, cmd.item);
-            break;
-        case TR_GOLD : 
-            transfer_gold(player,cmd.cantidad);
+        case ACTION_DEPOSIT_GOLD:
+            deposit_gold(player, cmd.cantidad);
             break;
         default:
             break;
     }
 }
-*/
--- server/Game/npc/npcBanker.h
+++ server/Game/npc/npcBanker.h
@@ -5,50 +5,32 @@
 #include "npcFriendly.h"
 #include <map>
 #include <vector>
+#include <memory>
+#include "../item/item.h"
+#include "../../../common/info/item_info.h"
 
 class Player;
 
 class NPCbanker: public NPCfriendly {
     private:
-        // an npc_id is generated and a name
-        NPCbanker();
-
         int pos_x;
         int pos_y;
-        /* interacts with player:
-         * - saves item from player
-         * - gives item to player
-         * - saves gold from player
-         * - give gold to player
-         */
 
-    public:
-    /*   
-    //<player_name, items_vector>
-        std::map<std::string, std::vector<Item>> item_bank;
-        //<player_name, gold>
+        std::map<std::string, std::vector<std::unique_ptr<Item>>> item_bank;
         std::map<std::string, int> gold_bank;
 
-        // gets item bank
-        std::map<int, std::vector<Item>> get_item_bank();
-        // gets gold bank
-        std::map<int, int> get_gold_bank();
-        // adds item bank
-        void receive_item(Player player, Item item);
-        // adds gold from player
-        void receive_gold(Player player, int ammount);
-        // transfers item to player and deletes it from bank
-        void transfer_item(Player player, Item item);
-        // transfers gold to player and subtracts it from bank
-        void transfer_gold(Player player, int ammount);
-    */
+        void deposit_item(Player& player, const std::string& item_id);
+        void deposit_gold(Player& player, int amount);
 
-        int get_coord_x() const { return pos_x; }
-        int get_coord_y() const { return pos_y; }
+    public:
+        NPCbanker(int x, int y);
+        int get_coord_x() const;
+        int get_coord_y() const; 
 
-        void interact(Player& player, Command cmd) override {};
-        
-        NPCbanker(int x, int y) : pos_x(x), pos_y(y) {}
+        std::vector<ItemInfo> list_bank(const std::string& player_name) const;
+
+        void interact(Player& player, Command cmd) override;
+
         NPCbanker(NPCbanker&&) = default;
         NPCbanker& operator=(NPCbanker&&) = default;
         NPCbanker(const NPCbanker&) = delete;
