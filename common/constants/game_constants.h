f3e81e38605eef1352858f855a6c19a366eb9d80
agus778 <agustintrombetta8@gmail.com>
2026-06-20 22:06:23 -0300

    update docs

--- common/constants/game_constants.h
+++ common/constants/game_constants.h
@@ -47,8 +47,7 @@ enum class elements {
 
 #define HEIGHT 16
 #define WIDTH 30
-// Tamano de tile en pixeles. Lo usan el editor grafico, el formato .bin y el
-// render. Fijo en V1 (mapas 30x16 con tiles de 64x64).
+// Tamaño de tile en pixeles. Lo usan el editor grafico, el formato .bin y el render.
 #define TILE_SIZE 64
 
 // ---- Persistencia de jugadores ----
